1d97de2b71eef27e21d793a1a6c0a3ca9dad37ec
kevin viglucci <kviglucci@gmail.com>
2019-09-07 15:35:58 -0500

    initial commit - hello world http

---
+++ src/main/java/com/solidice/springbootrsocketserver/SpringBootRsocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.solidice.springbootrsocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRsocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRsocketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solidice/springbootrsocketserver/SpringBootRsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solidice.springbootrsocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRsocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
