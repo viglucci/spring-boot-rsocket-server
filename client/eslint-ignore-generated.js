ccfda42b7fde104f562054d724490d525a385061
kevin viglucci <kviglucci@gmail.com>
2019-09-08 16:33:27 -0500

    reduce string duplication in script

--- client/eslint-ignore-generated.js
+++ client/eslint-ignore-generated.js
@@ -8,8 +8,8 @@ const files = fs.list(OUT_DIR);
 
 files.forEach((fileName) => {
 	const filePath = path.resolve(OUT_DIR, fileName);
-	console.log(`Appending /* eslint-disable */ to ${filePath}`);
-	const contents = fs.read(filePath);
 	const header = "/* eslint-disable */";
+	console.log(`Appending ${header} to ${filePath}`);
+	const contents = fs.read(filePath);
 	fs.write(filePath, `${header}${EOL}${contents}`);
 });
