b116f19a9531b58ec4e38dd97194c799406e2e78
kevin viglucci <kviglucci@gmail.com>
2019-09-08 21:13:03 -0500

    remove unused properties and rename main application file

--- src/main/java/com/solidice/springbootrsocketserver/SpringBootRSocketServerApplication.java
+++ src/main/java/com/solidice/springbootrsocketserver/SpringBootRSocketServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRsocketServerApplication {
+public class SpringBootRSocketServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootRsocketServerApplication.class, args);
+        SpringApplication.run(SpringBootRSocketServerApplication.class, args);
     }
 }
